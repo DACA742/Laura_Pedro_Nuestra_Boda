0e7a3b7f88382a798e8eb158e74334c86a4659cb
DACA742 <danielalejandrochavezarteaga@gmail.com>
2024-11-29 14:04:10 -0600

    t

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/Tatis_Chavez_Nuestra_Boda/',
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
